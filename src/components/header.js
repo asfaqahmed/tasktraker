2f8217d38b94ba6c750041891ef4af92efc5c856
asfaq <asfaqahmed356@gmail.com>
2022-12-18 11:02:34 +0530

    Task tracker initializing

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function header() {
+  return (
+    <div className='task'>
+      <h3>Task Title</h3>
+      <button>Add</button>
+    </div>
+  )
+}
+
+export default header
